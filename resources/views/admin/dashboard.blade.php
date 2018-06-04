c6101839b94badab5526a311ae4882df50ddd883
Amanzhol <qum.aman@gmail.com>
2018-06-04 07:06:18 +0600

    add Admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.layouts.app_admin')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <a href="#" class="list-group-item">
+                <h4 class="list-group-item-heading">Пользователи</h4>
+                <p class="list-group-item-text">Список</p>
+            </a>
+        </div>
+    </div>
+</div>
+@endsection
